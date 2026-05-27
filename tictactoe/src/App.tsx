1588dfc846b0b4f0836e5ea8a2360be7e8ee5979
milanr21 <milanrawal309@gmail.com>
2026-05-27 22:58:37 +0545

    react tailwind setup for tictactoe

---
+++ tictactoe/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-center  text-green-300 text-3xl'>
+      <h1>This is fucking react</h1>
+    </div>
+  );
+}
+
+export default App;
